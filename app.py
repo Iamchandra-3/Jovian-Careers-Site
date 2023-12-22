d3b577cc0c276c76cdd068c2c24ec8cc6e9f654b
Iamchandra-3 <77445002+Iamchandra-3@users.noreply.github.com>
2023-12-22 17:26:17 +0000

    Added Dynamic data and Api route

--- app.py
+++ app.py
@@ -2,10 +2,39 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+
+{
+
+    'id': 1,
+    'title': 'Python Developer',
+    'location': 'New York',
+    'salary': '$100,000'
+},
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'New York',
+    'salary': '$100,000'
+  },
+  {
+    'id': 3,
+    'title': 'Backend Engineer',
+    'location': 'Remote',
+    # 'salary': '$100,000'
+  },
+  {
+    'id': 4,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+    'salary': '$100,000'
+  }
+  
+]
 
 @app.route("/")
 def hello_jovian():
-  return render_template('home.html')
+  return render_template('home.html',jobs=JOBS)
 
 
 if __name__ == "__main__":
